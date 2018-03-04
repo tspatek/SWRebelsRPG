7e45b3f1a8c8685e5f911321b04ecb59cf9c3d4c
Tiffany Patek <tiffanypatek@Tiffanys-MacBook-Pro.local>
2018-03-04 16:12:18 -0700

    additional edits

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,156 +1,200 @@
 
-var game = {
-    player: {
-        id: "",
-        name: "",
-        img: "",
-        health: 0,
-        attack: 0,
-    },
-    defender: {
-        id: "",
-        name: "",
-        img: "",
-        health: 0,
-        counterAttack: 0
-    },
-    character: [
-        {
-            id: "kanan",
-            name: "Kanan Jarrus",
-            img: "assets/images/kanan.jpg",
-            health: 100,
-            attack: 9,
-            counterAttack: 20
+$(document).ready(function () {
+    var choosePlayer = true;
+    var idValue = " ";
+    var jqId = " ";
+
+    var game = {
+        player: {
+            health: 0,
+            baseAttack: 0,
+            attack: 0,
         },
-        {
-            id: "ahsoka",
-            name: "Ahsoka Tano",
-            img: "assets/images/ahsoka.jpg",
-            healthPoints: 125,
-            attackPower: 17,
-            counterAttackPower: 18
+        defender: {
+            health: 0,
+            counterAttack: 0,
         },
-        {
-            id: "inquisitor",
-            name: "Grand Inquisitor",
-            img: "assets/images/inquisitor.png",
-            healthPoints: 150,
-            attackPower: 13,
-            counterAttackPower: 6
-        },
-        {
-            id: "thrawn",
-            name: "Grand Admiral Thrawn",
-            img: "assets/images/thrawn.jpg",
-            healthPoints: 175,
-            attackPower: 11,
-            counterAttackPower: 14
-        }
-    ],
-    displayChar: function(container, charId, charName, charImg, charHealth) {
-        $(container).append(
-            `<div id=` + charId + ` class="character 
+        character: [
+            {
+                id: "kanan",
+                name: "Kanan Jarrus",
+                img: "assets/images/kanan.jpg",
+                health: 100,
+                baseAttack: 9,
+                attack: 9,
+                counterAttack: 20,
+                isPLayer: false,
+                isEnemy: false,
+                isDefender: false
+            },
+            {
+                id: "ahsoka",
+                name: "Ahsoka Tano",
+                img: "assets/images/ahsoka.jpg",
+                health: 125,
+                baseAttack: 17,
+                attack: 17,
+                counterAttack: 18,
+                isPLayer: false,
+                isEnemy: false,
+                isDefender: false
+            },
+            {
+                id: "inquisitor",
+                name: "Grand Inquisitor",
+                img: "assets/images/inquisitor.png",
+                health: 150,
+                baseAttack: 13,
+                attack: 13,
+                counterAttack: 6,
+                isPLayer: false,
+                isEnemy: false,
+                isDefender: false
+            },
+            {
+                id: "thrawn",
+                name: "Grand Admiral Thrawn",
+                img: "assets/images/thrawn.jpg",
+                health: 175,
+                baseAttack: 11,
+                attack: 11,
+                counterAttack: 14,
+                isPLayer: false,
+                isEnemy: false,
+                isDefender: false
+            }
+        ],
+        displayChar: function (container, charId, charName, charImg, charHealth) {
+            $(container).append(
+                `<div id=` + charId + ` class="character 
                     col-sm text-center mx-3">
                 <div class="p-1">` + charName + `</div>
                 <div>
-                    <img src=` + charImg + ` alt=` + charName + 
-                        ` height="150" width="150">
+                    <img src=` + charImg + ` alt=` + charName +
+                ` height="150" width="150">
                 </div>
                 <div class="p-1">` + charHealth + `</div>
             </div>`
-        );
-    },
-    pickPlayer: function (event) {
-        var idValue = $(this).attr("id");
-        var cssId = "#" + idValue;
+            );
+        },
+        pickPlayer: function (event) {
+            idValue = $(event.currentTarget).attr("id").trim();
 
-        game.character.forEach(function (obj) {
-            if (obj.id === idValue) {
-                game.player.id = obj.id;
-                game.player.name = obj.name;
-                game.player.img = obj.img;
-                game.player.health = obj.health;
-                game.player.attack = obj.attack;
+            game.character.forEach(function (obj) {
+                jqId = "#" + obj.id;
+
+                if (obj.id === idValue) {
+                    obj.isPLayer = true;
+                    game.player.health = obj.health;
+                    game.player.baseAttack = obj.baseAttack;
+                    game.player.attack = obj.attack;
+
+                    game.displayChar(
+                        "#player",
+                        obj.id,
+                        obj.name,
+                        obj.img,
+                        obj.health
+                    );
+                } else {
+                    obj.isEnemy = true;
+                    game.displayChar(
+                        "#avail-enemies",
+                        obj.id,
+                        obj.name,
+                        obj.img,
+                        obj.health
+                    );
+
+                    $(jqId).css("background-color", "#be5457");
+                    $(jqId).css("color", "#fefcf9");
+                }
+            });
+
+            $("#avail-players").empty();
+
+            // Append <h2>PLAYER</h2> to id="kanan-home"
+
+            // Change id avail players class attr to 
+            // align-items-start
+
+            // charaters without selected id are moved
+            // to Enemies area 
+
+            // Append <h2>AVAILABLE ENEMIES</h2> to
+            // id avial-enemies 
+
+            // Append <h2>DEFENDER</h2> to id="thrawn-home"
+
+        },
+        pickDefender: function (event) {
+            idValue = $(event.currentTarget).attr("id").trim();
+            jqId = "#" + idValue;
+            $(jqId).detach();
+
+            game.character.forEach(function (obj) {
+                if (obj.id === idValue && obj.isEnemy) {
+                    obj.isDefender = true;
+                    game.displayChar(
+                        "#defender",
+                        obj.id,
+                        obj.name,
+                        obj.img,
+                        obj.health
+                    );
+                    game.defender.health = obj.health;
+                    game.defender.counterAttack = obj.counterAttack;
+                }
+            });
+        },
+        pickChar: function (event) {
+            game.character.forEach(function (obj) {
+                if (obj.isPLayer) {
+                    choosePlayer = false;
+                } 
+            });
+            if (choosePlayer) {
+                game.pickPlayer(event);
+            } else {
+                game.pickDefender(event);
             }
-        });
+        },
+        attack: function (event) {
+            game.defender.health -= game.player.attack;
+            game.player.health -= game.defender.counterAttack;
+            game.player.attack += game.player.baseAttack;
 
-        $(cssId).detach();
+            game.character.forEach(function (obj) {
+                if (obj.isDefender) {
+                    obj.health = game.defender.health;
+                }
+                if (obj.isPLayer){
+                    obj.health = game.player.health;
+                    obj.attack = game.player.attack;
+                }
 
-        //character with selected id is moved
-        //to Player area
+            });
+        }
+    };
+
+    //Start Game
+    game.character.forEach(function (obj) {
         game.displayChar(
-            "#player", 
-            game.player.id,
-            game.player.name,
-            game.player.img,
-            game.player.health
+            "#avail-players",
+            obj.id,
+            obj.name,
+            obj.img,
+            obj.health
         );
+    });
 
-        $(cssId).css("background-color", "#be5457");
-        $(cssId).css("color", "#fefcf9");
+    $(".container").on("click", ".character", game.pickChar);
 
-        // Append <h2>PLAYER</h2> to id="kanan-home"
-
-        // Change id avail players class attr to 
-        // align-items-start
-
-        // charaters without selected id are moved
-        // to Enemies area 
-
-        // Append <h2>AVAILABLE ENEMIES</h2> to
-        // id avial-enemies 
-
-        // Append <h2>DEFENDER</h2> to id="thrawn-home"
-
-        // find character object with selected 
-        // id and assign to player 
-
-    }
-    // attack: function () {
-    //     //triggered by attack button press
-    // },
-    // restart: function () {
-    //     location.reload(true);
-    // }
-};
-
-$(document).ready(function () {
-    game.displayChar(
-        "#avail-players", 
-        game.character[0].id,
-        game.character[0].name,
-        game.character[0].img,
-        game.character[0].health
-    );
-    game.displayChar(
-        "#avail-players", 
-        game.character[1].id,
-        game.character[1].name,
-        game.character[1].img,
-        game.character[1].health
-    );
-    game.displayChar(
-        "#avail-players", 
-        game.character[2].id,
-        game.character[2].name,
-        game.character[2].img,
-        game.character[2].health
-    );
-    game.displayChar(
-        "#avail-players", 
-        game.character[3].id,
-        game.character[3].name,
-        game.character[3].img,
-        game.character[3].health
-    );
-
-    $(".character").on("click", game.pickPlayer);
-
-    $("#attack").on("click", function () {
+    $("attack-button").on("click", function () {
 
     });
 
-    $("#reset").on("click", function () {
+    $("button").on("click", "#restart-button" function () {
+
     });
 });
